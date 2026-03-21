c7265b101bf6b981b0b935c87ad5e316ca3b07e7
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-03-21 22:37:31 +0800

    added test file for array homework

---
+++ Object-oriented_analysis_and_programming/Array/test1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "array.h"
+using namespace std;
+
+int main()
+{
+    Array test1(10);
+
+    cout << test1 << endl;
+    cout << test1.getSize() << endl;
+    cout << "---" << endl;
+
+    test1.insert(1);
+    test1.insert(2);
+    test1.insert(3, 1);
+    cout << test1 << endl;
+    test1.remove(0);
+    cout << test1 << endl;
+    cout << test1[0] << endl;
+
+    return 0;
+}
